f456b01f89087dcb68ed07e661e42257196967d8
SausageMuffins <rwang9098@gmail.com>
2023-08-30 13:13:57 +0800

    hands on with conditions

---
+++ conditions.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int number = 10;
+
+    if (number==10) {
+        printf("The number is 10");	
+    } else if ( number > 10) {
+        printf("The number is greater than 10");
+    } else {
+        printf("The number is less than 10");
+    };
+
+    return 0;
+}
